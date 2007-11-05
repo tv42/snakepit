894c47c6c1a58b432f88cf418216145cd47c3dfa
Tommi Virtanen <tv@eagain.net>
2007-11-05 19:41:03 +0200

    Get reliable test names by finding nose in callstack and brutally attacking it.

--- snakepit/test/util.py
+++ snakepit/test/util.py
@@ -12,28 +12,41 @@ def mkdir(*a, **kw):
         else:
             raise
 
+def find_test_name():
+    try:
+        from nose.case import Test
+        def get_nose_name(its_self):
+            if isinstance(its_self, Test):
+                file_, module, class_ = its_self.address()
+                name = '%s:%s' % (module, class_)
+                return name
+    except ImportError:
+        # older nose
+        from nose.case import FunctionTestCase, MethodTestCase
+        from nose.util import test_address
+        def get_nose_name(its_self):
+            if isinstance(its_self, (FunctionTestCase, MethodTestCase)):
+                file_, module, class_ = test_address(its_self)
+                name = '%s:%s' % (module, class_)
+                return name
+
+    i = 0
+    while True:
+        i += 1
+        frame = sys._getframe(i)
+        # kludge, hunt callers upwards until we find our nose
+        if (frame.f_code.co_varnames
+            and frame.f_code.co_varnames[0] == 'self'):
+            its_self = frame.f_locals['self']
+            name = get_nose_name(its_self)
+            if name is not None:
+                return name
+
 def maketemp():
     tmp = os.path.join(os.path.dirname(__file__), 'tmp')
     mkdir(tmp)
 
-    caller = sys._getframe(1)
-
-    # kludgy way to detect methods, if their first arg is called
-    # "self"
-    if (caller.f_code.co_varnames
-        and caller.f_code.co_varnames[0] == 'self'):
-        its_self = caller.f_locals['self']
-        name = '%s.%s.%s' % (
-            its_self.__class__.__module__,
-            its_self.__class__.__name__,
-            caller.f_code.co_name,
-            )
-    else:
-        name = '%s.%s' % (
-            sys._getframe(1).f_globals['__name__'],
-            caller.f_code.co_name,
-            )
-
+    name = find_test_name()
     tmp = os.path.join(tmp, name)
     try:
         shutil.rmtree(tmp)
